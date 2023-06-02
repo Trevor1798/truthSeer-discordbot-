af6529c30b615e7718ddee9b8b5104f9ead938ce
Trevor1798 <Trevor1798@yahoo.com>
2023-06-01 21:13:45 -0400

    working on dependency issues

--- voice_recognition_commands.py
+++ voice_recognition_commands.py
@@ -2,22 +2,23 @@ import discord
 from discord.ext import commands
 import os
 import speech_recognition as sr
+import asyncio
 
 intents = discord.Intents.default()
 intents.voice_states = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Function to encapsulate speech, call it in the command functions
-def recognize_speech():
+# Function to encapsulate speech recognition
+async def recognize_speech():
+    loop = asyncio.get_event_loop()
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        audio = r.listen(source)
+        audio = await loop.run_in_executor(None, r.listen, source)
 
-    # Perform speech recognition on the captured audio
     try:
-        text = r.recognize_google(audio)
+        text = await loop.run_in_executor(None, r.recognize_google, audio)
         return text
     except sr.UnknownValueError:
         print("Speech recognition could not understand audio")
@@ -38,7 +39,7 @@ async def on_voice_state_update(member, before, after):
         # User has joined a voice channel
         if member == bot.user:
             # Bot has joined the voice channel
-            speech_text = recognize_speech()
+            speech_text = await recognize_speech()
 
             if "Yooo Jarvis mute these" in speech_text:
                 channel = after.channel
@@ -59,7 +60,6 @@ async def on_voice_state_update(member, before, after):
             else:
                 print("Invalid command")
 
-@bot.command()
 async def join(ctx):
     if ctx.author.voice is None:
         await ctx.send("You must be in a voice channel to use this command.")
@@ -68,11 +68,29 @@ async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
 
+    # Call speech recognition function when the bot joins a voice channel
+    speech_text = await recognize_speech()
+
+    if "Yooo Jarvis mute these" in speech_text:
+        for member in channel.members:
+            if member != bot.user:
+                await member.edit(mute=True)
+
+        print("Muted everyone")
+    elif "Jarvis unmute" in speech_text:
+        for member in channel.members:
+            if member != bot.user:
+                await member.edit(mute=False)
+
+        print("Unmuted everyone")
+    else:
+        print("Invalid command")
+
+
 @bot.command()
 async def leave(ctx):
     voice_client = ctx.guild.voice_client
     if voice_client is not None:
         await voice_client.disconnect()
 
-
-
+bot.run("YOUR_BOT_TOKEN")
